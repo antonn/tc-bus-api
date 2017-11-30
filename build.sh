11d674b310312d9e905624d888832e5eb2046115
Nkumar <nagendrakumar.mruthyunja@topcoder.com>
2017-11-30 18:49:18 +0530

    build 18

--- build.sh
+++ build.sh
@@ -18,35 +18,26 @@ LOG_LEVEL=LOG_LEVEL=$(eval "echo \$${ENV}_LOG_LEVEL")
 NODE_PORT=NODE_PORT=$(eval "echo \$${ENV}_NODE_PORT")
 JWT_SECRET=JWT_SECRET=$(eval "echo \$${ENV}_JWT_SECRET")
 KAFKA_TOPIC_PREFIX=KAFKA_TOPIC_PREFIX=$(eval "echo \$${ENV}_KAFKA_TOPIC_PREFIX")
-#KAFKA_GROUP_ID=KAFKA_GROUP_ID=$(eval "echo \$${ENV}_KAFKA_GROUP_ID")
 API_VERSION=API_VERSION=$(eval "echo \$${ENV}_API_VERSION")
 ALLOWED_SERVICES=ALLOWED_SERVICES=$(eval "echo \$${ENV}_ALLOWED_SERVICES")
 JWT_TOKEN_EXPIRES_IN=JWT_TOKEN_EXPIRES_IN=$(eval "echo \$${ENV}_JWT_TOKEN_EXPIRES_IN")
 
-KAFKA_URL1=KAFKA_URL=$(eval "echo \$${ENV}_KAFKA_URL")
+KAFKA_URL=KAFKA_URL=$(eval "echo \$${ENV}_KAFKA_URL")
 KAFKA_CLIENT_CERT=KAFKA_CLIENT_CERT=$(eval "echo \$${ENV}_KAFKA_CLIENT_CERT")
 KAFKA_CLIENT_CERT_KEY=KAFKA_CLIENT_CERT_KEY=$(eval "echo \$${ENV}_KAFKA_CLIENT_CERT_KEY")
 
 #append kafka env to shell script
 
 #cat > envsh.sh <<< $KAFKA_URL$'\n'$KAFKA_CLIENT_CERT$'\n'$KAFKA_CLIENT_CERT_KEY
-printf '%s\n%s\n%s' 'export '$KAFKA_URL1 'export '$KAFKA_CLIENT_CERT 'export '$KAFKA_CLIENT_CERT_KEY | tee -a envsh.sh
-chmod 777 envsh.sh
-echo "==================displaying contents of shell script ====================== \n\n"
-cat envsh.sh
-
-echo "====================end =======================\n\n"
+#printf '%s\n%s\n%s' 'export '$KAFKA_URL1 'export '$KAFKA_CLIENT_CERT 'export '$KAFKA_CLIENT_CERT_KEY | tee -a envsh.sh
+#chmod 777 envsh.sh
 
 #append environment variable into .env file.
-KAFKA_URL=KAFKA_URL=$(eval "echo \$${ENV}_KAFKA_URL")
 printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n' $LOG_LEVEL $NODE_PORT $JWT_SECRET $KAFKA_URL $KAFKA_TOPIC_PREFIX $API_VERSION $ALLOWED_SERVICES $JWT_TOKEN_EXPIRES_IN | tee -a .env
-
-#echo "displaying contents of .env \n\n"
-#cat .env
 
 # Builds Docker image of the app.
 TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tc-bus-api:$CIRCLE_SHA1
-#TAG=community-app:$CIRCLE_SHA1
+
 docker build -t $TAG \
   --build-arg NODE_ENV=$NODE_ENV .
 
