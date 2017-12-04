e9c26156d83e67ad4c0812fbecff898db24a0aab
Nkumar <nagendrakumar.mruthyunja@topcoder.com>
2017-12-04 17:57:20 +0530

    new build

--- config/default.js
+++ config/default.js
@@ -2,16 +2,30 @@
  * The default configuration.
  */
 
+console.log(' process.env.LOG_LEVEL  :', process.env.LOG_LEVEL)
+console.log(' CONTEXT_PATH API VERSION :', process.env.API_VERSION)
+console.log(' process.env.ALLOWED_SERVICES  :', process.env.ALLOWED_SERVICES)
+console.log(' process.env.JWT_TOKEN_SECRET  :', process.env.JWT_TOKEN_SECRET)
+console.log(' process.env.KAFKA_TOPIC_PREFIX  :', process.env.KAFKA_TOPIC_PREFIX)
+
 module.exports = {
-  LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
-  CONTEXT_PATH: process.env.CONTEXT_PATH || '/api/v1',
-  PORT: process.env.PORT || 3000,
-  ALLOWED_SERVICES: process.env.ALLOWED_SERVICES || [
-    'project-service',
-    'challenge-service',
-    'message-service'
-  ],
-  JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET || 'JWT_TOKEN_SECRET',
-  JWT_TOKEN_EXPIRES_IN: process.env.JWT_TOKEN_EXPIRES_IN || '100 days',
-  KAFKA_TOPIC_PREFIX: process.env.KAFKA_TOPIC_PREFIX || 'joan-26673.notifications.'
+  //LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
+  //CONTEXT_PATH: process.env.CONTEXT_PATH || '/api/v1',
+  //PORT: process.env.PORT || 3000,
+  //ALLOWED_SERVICES: process.env.ALLOWED_SERVICES || [
+    //'project-service',
+    //'challenge-service',
+    //'message-service'
+  //],
+  //JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET || 'JWT_TOKEN_SECRET',
+  //JWT_TOKEN_EXPIRES_IN: process.env.JWT_TOKEN_EXPIRES_IN || '100 days',
+  //KAFKA_TOPIC_PREFIX: process.env.KAFKA_TOPIC_PREFIX || 'joan-26673.notifications.'
+
+  LOG_LEVEL: process.env.LOG_LEVEL,
+  CONTEXT_PATH: process.env.API_VERSION,
+  PORT: process.env.PORT,
+  ALLOWED_SERVICES: process.env.ALLOWED_SERVICES,
+  JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET,
+  JWT_TOKEN_EXPIRES_IN: process.env.JWT_TOKEN_EXPIRES_IN,
+  KAFKA_TOPIC_PREFIX: process.env.KAFKA_TOPIC_PREFIX
 }
