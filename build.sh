6d7571dff7ca67832efdd079a725cc93c57cef22
Nkumar <na388291@L-156161608.local>
2018-09-27 15:23:33 +0530

    update

--- build.sh
+++ build.sh
@@ -1,46 +1,49 @@
 #!/bin/bash
 set -eo pipefail
 
-
-# Builds Docker image of tc-bus-api application.
-# This script expects a single argument: NODE_ENV, which must be either
-# "development" or "production".
-
 NODE_ENV=$1
-
 ENV=$1
 AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
 AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
 
+if [[ -z "$ENV" ]] ; then
+	echo "Environment should be set on startup with one of the below values"
+	echo "ENV must be one of - DEV, QA, PROD or LOCAL"
+	exit
+fi
 
-# Builds Docker image of the app.
-TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tc-bus-api:$CIRCLE_SHA1
+LOG_LEVEL=$(eval "echo \$${ENV}_LOG_LEVEL")
+JWT_TOKEN_SECRET=$(eval "echo \$${ENV}_JWT_TOKEN_SECRET")
+API_VERSION=$(eval "echo \$${ENV}_API_VERSION")
+PORT=$(eval "echo \$${ENV}_NODE_PORT")
 
-docker build -t $TAG .
+KAFKA_URL=$(eval "echo \$${ENV}_KAFKA_URL")
+KAFKA_CLIENT_CERT=$(eval "echo \$${ENV}_KAFKA_CLIENT_CERT")
+KAFKA_CLIENT_CERT_KEY=$(eval "echo \$${ENV}_KAFKA_CLIENT_CERT_KEY")
 
-# Copies "node_modules" from the created image, if necessary for caching.
-docker create --name app $TAG
+VALID_ISSUERS=$(eval "echo \$${ENV}_VALID_ISSUERS")
 
- if [ -d node_modules ]
- then
-   # If "node_modules" directory already exists, we should compare
-   # "package-lock.json" from the code and from the container to decide,
-   # whether we need to re-cache, and thus to copy "node_modules" from
-   # the Docker container.
-   mv package-lock.json old-package-lock.json
-   docker cp app:/opt/app/package-lock.json package-lock.json
-   set +eo pipefail
-   UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
-   set -eo pipefail
- else
-    # If "node_modules" does not exist, then cache must be created.
-   UPDATE_CACHE=1
- fi
+TC_EMAIL_SERVICE_URL=$(eval "echo \$${ENV}_TC_EMAIL_SERVICE_URL")
 
- if [ "$UPDATE_CACHE" == 1 ]
- then
-   docker cp app:/opt/app/node_modules .
- fi
+AUTH0_URL=$(eval "echo \$${ENV}_AUTH0_URL")
+AUTH0_AUDIENCE=$(eval "echo \$${ENV}_AUTH0_AUDIENCE")
+TOKEN_CACHE_TIME=$(eval "echo \$${ENV}_TOKEN_CACHE_TIME")
+AUTH0_CLIENT_ID=$(eval "echo \$${ENV}_AUTH0_CLIENT_ID")
+AUTH0_CLIENT_SECRET=$(eval "echo \$${ENV}_AUTH0_CLIENT_SECRET")
+
+template='{"API_VERSION":"%s","AUTH0_AUDIENCE":"%s","AUTH0_CLIENT_ID":"%s","AUTH0_CLIENT_SECRET":"%s","AUTH0_URL":"%s","ENV":"%s","LOG_LEVEL":"%s","JWT_TOKEN_SECRET":"%s","KAFKA_URL":"%s","KAFKA_CLIENT_CERT":"%s","KAFKA_CLIENT_CERT_KEY":"%s","TC_EMAIL_SERVICE_URL":"%s","TOKEN_CACHE_TIME": "%s", "VALID_ISSUERS": %s}'
+   
+json_string=$(printf "$template" "$API_VERSION" "$AUTH0_AUDIENCE" "$AUTH0_CLIENT_ID" "$AUTH0_CLIENT_SECRET" "$AUTH0_URL" "$ENV" "$LOG_LEVEL" "$JWT_TOKEN_SECRET" "$KAFKA_URL"  "$KAFKA_CLIENT_CERT" "$KAFKA_CLIENT_CERT_KEY" "$TC_EMAIL_SERVICE_URL" "$TOKEN_CACHE_TIME" "$VALID_ISSUERS")
+
+echo $json_string > .config/dev.json
+
+configure_aws_cli() {
+	aws --version
+	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+	aws configure set default.region $AWS_REGION
+	aws configure set default.output json
+	echo "Configured AWS CLI."
+}
