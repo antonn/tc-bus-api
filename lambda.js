356e1ebdace0907dea661825619c236f163c89d3
sachin-maheshwari <sachin.maheshwari@rediffmail.com>
2018-10-12 13:54:42 +0530

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -3,4 +3,9 @@ const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./index')
 const server = awsServerlessExpress.createServer(app)
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+exports.handler = (event, context) => { 
+  console.log("events", event)
+  console.log("context", context)
+  awsServerlessExpress.proxy(server, event, context)
+  
+}
