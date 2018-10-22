5612164182a2efb7735dd0e69bddee05811f0045
sachin-maheshwari <sachin.maheshwari@rediffmail.com>
2018-10-22 19:40:22 +0530

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,6 +1,7 @@
 'use strict'
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./index')
+//gzip
 const binaryMimeTypes = ['gzip']
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
 exports.handler = (event, context) => { 
