a6da0837d689555884e49d5714c82402e285ac4a
sachin-maheshwari <sachin.maheshwari@rediffmail.com>
2018-11-29 11:39:55 +0530

    removing content-encoding header

--- lambda.js
+++ lambda.js
@@ -1,12 +1,24 @@
 'use strict'
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./index')
-//gzip
-const binaryMimeTypes = ['gzip', 'application/gzip']
-const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
+const server = awsServerlessExpress.createServer(app, null)
 exports.handler = (event, context) => { 
-  console.log("events", event)
-  console.log("context", context)
+  console.log("before events", event)
+  console.log("before context", context)
+  if (event.headers) {
+        let headers = event.headers
+        if (headers['Content-Encoding']) {
+            delete headers['Content-Encoding']
+        }
+        if (event.multiValueHeaders) {
+            if (event.multiValueHeaders['Content-Encoding']) {
+               delete event.multiValueHeaders['Content-Encoding'] 
+            }
+        }
+
+   }
+  console.log("after events", event)
+  console.log("after context", context)
   return awsServerlessExpress.proxy(server, event, context)
   
 }
